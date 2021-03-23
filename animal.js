a325f971f2a7fb96a2f9a6d70d87bd4b8357bd3a
Jenny Wang <Jeanyyue@MacBook-Pro.local>
2021-03-23 10:06:03 -0700

    Lab 8 Update

--- animal.js
+++ animal.js
@@ -52,12 +52,47 @@ function generateRandomAge() {
 }
 
 function onLoad() {
-    let animal = generateRandomAnimal();
-    console.log(animal)
-        // update the page based on the animal properties
-    document.getElementById('animal-properties').textContent = animal.name + "  " + animal.age + "years old";
-    let imageTag = document.getElementById('animal-img');
-    imageTag.setAttribute('src', animal.image);
-    imageTag.setAttribute('alt', animal.image_alt);
+    var animal = JSON.parse(localStorage.getItem("MyAnimal"));
 
-}
+    var SavedAnimal = false;
+
+    if (animal === null) {
+        document.getElementById("storage").textContent = "Save Animal";
+
+        animal = generateRandomAnimal();
+    } else {
+        document.getElementById("storage").textContent = "Clear Saved Animal";
+        SavedAnimal = true;
+    }
+    //Updates Webpage
+    document.getElementById('animal-properties').textContent = animal.name + "  " + animal.age + "years old";
+    document.getElementById('animal-img').setAttribute("src", animal.image)
+
+    document.getElementById("storage").addEventListener("click", function() {
+        if (SavedAnimal) {
+
+            localStorage.removeItem("MyAnimal");
+            document.getElementById('storage').style.display = "none";
+            document.getElementById("button-text").textContent = "Woo-hoo!Cleared!";
+            document.getElementById("button-text").style.display = "block";
+
+
+        } else {
+            localStorage.setItem("MyAnimal", JSON.stringify(animal));
+            document.getElementById('storage').style.display = "none";
+            document.getElementById("button-text").textContent = "Woo-hoo!Saved!";
+            document.getElementById("button-text").style.display = "block";
+        }
+    });
+};
+
+
+//    let animal = generateRandomAnimal();
+//            console.log(animal)
+// update the page based on the animal properties
+//            document.getElementById('animal-properties').textContent = animal.name + "  " + animal.age + "years old";
+//           let imageTag = document.getElementById('animal-img');
+//            imageTag.setAttribute('src', animal.image);
+//            imageTag.setAttribute('alt', animal.image_alt);
+
+//        }
